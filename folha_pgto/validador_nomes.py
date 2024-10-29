54e139c06cdeefb897d95eafab82b3a162299dbb
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-10-28 22:49:55 -0300

    teste com partial_ratio

--- folha_pgto/validador_nomes.py
+++ folha_pgto/validador_nomes.py
@@ -6,29 +6,51 @@ def comparar_nomes(baseAnalise, baseCorreta):
 
     parNomes = {}
     for nomeBaseAnalise in baseAnalise:
-        similaridadesLista = [] # Lista da similaridade de "nomeBaseAnalise" com todos os nomes em "BaseCorreta"
-        nomesSimilares = [] # Lista com os nomes mais similares
+        # Dicionário com as similaridades entre os nomes da baseAnalise em relação a baseCorreta
+        similaridadesBaseAnalise = []
         
         for nomeBaseCorreta in baseCorreta:
             similaridade = fuzz.ratio(nomeBaseAnalise, nomeBaseCorreta)
-            similaridadesLista.append(similaridade)
-            nomesSimilares.append(nomeBaseCorreta)
+            similaridadePartial = fuzz.partial_ratio(nomeBaseAnalise, nomeBaseCorreta)
+            similaridadesBaseAnalise.append((nomeBaseCorreta, (similaridade, similaridadePartial)))
         
-        maiorSimilaridade = max(similaridadesLista)
+        doisMaisSimilares = sorted(similaridadesBaseAnalise,
+                               key=lambda item: item[1][0],
+                               reverse=True)[:2]
+        
+        nomeMaisSimilar, _ = sorted(doisMaisSimilares,
+                               key=lambda item: item[1][1],
+                               reverse=True)[0]
 
-        for nome in nomesSimilares:
-            similaridade = fuzz.ratio(nomeBaseAnalise, nome)
-            if similaridade == maiorSimilaridade:
-                nomeMaisSimilar = nome
+        # for nome in nomesSimilares:
+        #     similaridade = fuzz.ratio(nomeBaseAnalise, nome)
+        #     if similaridade == maiorSimilaridade:
+        #         nomeMaisSimilar = nome
 
         parNomes[nomeBaseAnalise] = nomeMaisSimilar
     return parNomes
 
 
 if __name__ == '__main__':
-    print(fuzz.ratio('ROSELAINE SANTOS', 'JOSELAINE DOS SANTOS'))
-    print(fuzz.ratio('JOSELAINE DOS SANTOS', 'ROSELAINE SANTOS'))
-    print(fuzz.ratio('ROSELAINE SANTOS', 'ROSELAINE SANTOS AGUIRRE'))
+    # print(fuzz.ratio('ROSELAINE SANTOS', 'JOSELAINE DOS SANTOS'))
+    # print(fuzz.ratio('JOSELAINE DOS SANTOS', 'ROSELAINE SANTOS'))
+    # print(fuzz.ratio('ROSELAINE SANTOS', 'ROSELAINE SANTOS AGUIRRE'))
 
-    # print(fuzz.partial_ratio('ROSELAINE SANTOS', 'JOSELAINE DOS SANTOS'))
-    # print(fuzz.partial_ratio('ROSELAINE SANTOS', 'ROSELAINE SANTOS AGUIRRE'))
+    # Dicionário de exemplo
+    dicionario = {
+        "a": (10, 5),
+        "b": (15, 8),
+        "c": (7, 3),
+        "d": (20, 9),
+        "e": (25, 4)
+    }
+
+    # Ordena os itens com base no índice 0 da tupla em ordem decrescente e pega os dois primeiros
+    maiores = sorted(dicionario.items(), key=lambda item: item[1][0], reverse=True)[:2]
+    # maiores = sorted(dicionario.items(), key=lambda item: item[1][0], reverse=True)
+    print(maiores)
+    # Exibe o resultado
+    # for chave, valor in maiores:
+    #     print(f"Chave: {chave}, Valor: {valor}")
+
+
