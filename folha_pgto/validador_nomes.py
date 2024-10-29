02da61521d6a8b0470637c703341daa97922662e
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-10-28 23:56:42 -0300

    Ajustes adicionais no código

--- folha_pgto/validador_nomes.py
+++ folha_pgto/validador_nomes.py
@@ -26,19 +26,3 @@ def comparar_nomes(baseAnalise, baseCorreta):
                                      else segundoMaisSimilar[0])
 
     return parNomes
-
-
-if __name__ == '__main__':
-    import unicodedata
-
-    def remover_acentuacao(texto):
-        # Normaliza o texto para decompor caracteres acentuados
-        texto_normalizado = unicodedata.normalize('NFKD', texto)
-        # Filtra apenas caracteres ASCII (sem acentuação)
-        texto_sem_acentos = ''.join(c for c in texto_normalizado if not unicodedata.combining(c))
-        return texto_sem_acentos
-
-    # Exemplo de uso
-    texto = "Elisângela"
-    texto_sem_acentos = remover_acentuacao(texto)
-    print(texto_sem_acentos)
