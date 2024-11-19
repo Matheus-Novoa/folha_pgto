95dc18dbedf862b34aed6461114d604bcafb0fdf
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-11-19 10:11:50 -0300

    Correção na expressão regular e no caminho da planilha

--- folha_pgto/ler_folha_pgto.py
+++ folha_pgto/ler_folha_pgto.py
@@ -8,8 +8,11 @@ from pathlib import Path
 
 
 arquivo = Path(filedialog.askopenfilename())
-dadosFuncionarios = pd.read_excel('_internal/Empregados.xls', 'dados')
-# dadosFuncionarios = pd.read_excel('Empregados.xls', 'dados')
+
+pasta_atual = Path(__file__).parent
+# Procurar pelo arquivo na pasta atual e em subpastas
+arquivo_planilha = next(pasta_atual.rglob('Empregados.xls'), None)
+dadosFuncionarios = pd.read_excel(arquivo_planilha, 'dados')
 dadosFuncionarios['Nome'] = dadosFuncionarios['Nome'].str.strip()
 
 try:
@@ -38,10 +41,10 @@ except:
         for page in pdf.pages:
             textoBruto += page.extract_text()
 
-    padrao = r'(\d{2}/\d{2}/\d{4})\s+(\d{1,3}(?:\.\d{3})*,\d{2})(\(cid:9\)|\s)([^\d(]*)'
+    padrao = r"(\d{2}/\d{2}/\d{4}(\(cid:9\))?)\s+(\d{1,3}(?:\.\d{3})*,\d{2})(\(cid:9\)|\s)([^\d(]*)"
     dados = re.findall(padrao, textoBruto)
 
-    paresNomeValor = {linha[-1].strip():[linha[1]] for linha in dados}
+    paresNomeValor = {linha[-1].strip():[linha[2]] for linha in dados}
 
     tabelaDados = pd.DataFrame(paresNomeValor).T.reset_index()
     tabelaDados.columns = ['Nome', 'Valor']
